48f17d9d72758de6396bf4b08711ca91f1ce9057
Culty48 <robcultraro@gmail.com>
2024-03-18 01:06:17 -0400

    Added routes for most CRUD methods (no update)

--- restservice/src/main/java/com/yorku/library/restservice/DatabaseTester.java
+++ restservice/src/main/java/com/yorku/library/restservice/DatabaseTester.java
@@ -1,16 +1,12 @@
 package com.yorku.library.restservice;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.yorku.library.restservice.Entities.Item;
-import com.yorku.library.restservice.Entities.User;
-import com.yorku.library.restservice.Repositories.ItemRepo;
-import com.yorku.library.restservice.Repositories.UserRepo;
+import com.yorku.library.restservice.repositories.ItemRepo;
+import com.yorku.library.restservice.repositories.UserRepo;
 
 @SpringBootApplication
 public class DatabaseTester implements CommandLineRunner{
@@ -27,11 +23,8 @@ public class DatabaseTester implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception{
     	
-    	User user1 = new User("alex", "1234", "mail6"); //change email before testing
-        userRepo.save(user1);
-    	userRepo.findByUsername("alex").forEach(user -> System.out.println(user));
-    	Item item1 = new Item("king4", "king is my goat", "here"); //change item name before testing
-    	itemRepo.save(item1);
-    	itemRepo.findByName("king4").forEach(item -> System.out.println(item));
+    	userRepo.findAll().forEach(user -> System.out.println(user));
+    	itemRepo.findAll().forEach(item -> System.out.println(item));
+    	
     }
 }
--- restservice/src/main/java/com/yorku/library/restservice/controller/UserController.java
+++ restservice/src/main/java/com/yorku/library/restservice/controller/UserController.java
@@ -1,5 +1,68 @@
 package com.yorku.library.restservice.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yorku.library.restservice.entities.User;
+import com.yorku.library.restservice.repositories.UserRepo;
+
+@RestController
 public class UserController {
 
+	@Autowired
+    private UserRepo userRepo;
+	
+	@GetMapping("/users") 
+	public ResponseEntity<List<User>> getAllUsers() {
+	
+		return new ResponseEntity<>(userRepo.findAll(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/users/id/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Integer id) throws Exception {
+
+        Optional<User> user = userRepo.findById(id);
+        if (user.isPresent()) {
+            return new ResponseEntity<User>(user.get(), HttpStatus.OK);
+        } 
+        else {
+            throw new Exception("User Doesnt Exist");
+        }
+	}
+	
+	@GetMapping("/users/{name}")
+	public ResponseEntity<List<User>> getUsersByName(@PathVariable String name) throws Exception {
+		List<User> user = userRepo.findByUsername(name);
+        if (user.size() != 0) {
+            return new ResponseEntity<List<User>>(user, HttpStatus.OK);
+        } 
+        else {
+            throw new Exception("User Doesnt Exist");
+        }
+	}
+	
+	@PostMapping(path = "users")
+	public ResponseEntity<User> addUser(@RequestParam String username, @RequestParam String pw, @RequestParam String email) {
+		User user = new User(username, pw, email);
+		userRepo.save(user);
+		return new ResponseEntity<User>(user, HttpStatus.CREATED);
+	}
+	
+	//idk if this is right, tentative method
+	@DeleteMapping("users/delete/{id}")
+	public String deleteUser(@PathVariable Integer id) {
+		userRepo.deleteById(id);
+		return "User " + id + " Deleted";
+	}
 }
